02d2b1c2a8d3e8805d4ece38b5ea1ff40c358155
mhndakbar <makbar@mrsool.co>
2022-02-13 16:21:57 +0300

    Add user mailer preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+    def new_user_mailer
+        user = User.new(name:"Mohanned", email:"mrmohandtv@gmail.com")
+        UserMailer.with(user: user).new_user_email
+    end
 
 end
