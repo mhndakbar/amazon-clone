1e5c2fb6b4119b96b98efe9ef3dddd7a131e1171
mhndakbar <makbar@mrsool.co>
2022-02-13 16:21:12 +0300

    Send email for each new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       if @item.save
+        ItemMailer.with(item: @item).new_item_email.deliver_now
         format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
         format.json { render :show, status: :created, location: @item }
       else
