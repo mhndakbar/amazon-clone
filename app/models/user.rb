ed95c66e2995cf20ea448dc349b88265a55a0f61
mhndakbar <makbar@mrsool.co>
2022-02-16 15:33:54 +0300

    Add admin email job

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     after_create do
         SendUserEmailJob.perform_later(self)
+        SendSummaryMailJob.perform_later()
         puts "after create"
     end
 end
