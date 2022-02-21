08f12ec1dff2995583f9f5c07f4cbf0cf3317292
mhndakbar <makbar@mrsool.co>
2022-02-21 09:03:35 +0300

    Testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     after_create do
         SendUserEmailJob.perform_later(self)
         SendSummaryMailJob.perform_later()
-        puts "after create"
+        #puts "after create"
     end
 end
