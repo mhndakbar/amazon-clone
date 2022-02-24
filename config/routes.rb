f8093e3a5365f5f93f77352eb0713e9385afd746
mhndakbar <makbar@mrsool.co>
2022-02-24 09:33:29 +0300

    Configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :items do
     member do
       get :delete_attachment
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "application#home"
 end
